9223779e03e1db9d9414b02d398a7c0d1b3af092
GidenV <gabrieldvolpi@gmail.com>
2022-09-05 14:10:08 -0300

    First Commit

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
   // your code here
+  if(number == 0 || number == 1){
+    return 1;
+  }else{
+    return number * factorial(number-1);
+  }
 }
+// let number = 8;
+// console.log("result " + factorial(number));
 
 module.exports = factorial;
