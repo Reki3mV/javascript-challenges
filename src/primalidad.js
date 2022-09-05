202ac1cbe9be0c27fd8c920536441716e0cca5d6
GidenV <gabrieldvolpi@gmail.com>
2022-09-05 19:57:57 -0300

    Third

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
   // your code here
+  if (!Number.isInteger(number)) {
+    return false;
+  }
+  if (number<=1) return false;
+
+  for (let i = 2; i <= Math.sqrt(number); i++)
+      if (number % i == 0) return false;
+
+  return true;
 }
 
 module.exports = trialDivision;
