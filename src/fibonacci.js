58e698c9d5bb0bac72b5baf2e72273bbdaab61b2
GidenV <gabrieldvolpi@gmail.com>
2022-09-05 15:08:20 -0300

    Second

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,21 @@
 const fibonacci = (n) => {
   // your code here
-}
+  const fib = [1,1];
+
+  if(n==1){
+    console.log("si n es 1 devuelve " + n);
+    return [n];
+  }
+  else if(n==2){
+    console.log("si n es 2 devuelve " + fib);
+    return fib
+  }else{
+    for(let i = 2; i < n; i++){
+      fib[i] = fib[i-1] + fib[i-2];
+    }
+  }
+  console.log(fib)
+  return fib
+};
 
 module.exports = fibonacci;
